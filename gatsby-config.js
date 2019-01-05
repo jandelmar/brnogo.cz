28ce4dd51999f8451f2248acfa5d163faf506f78
Jan Delmar <12587320+jandelmar@users.noreply.github.com>
2019-01-05 18:45:13 +0100

    Revert "Merge from netlify-cms-test setup"
    This reverts commit ec2a4c500bd1adbd226440da44b8d78f9984f9fc.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,14 +12,14 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'jidlo',
-        path: `${__dirname}/content/jidlo/`
+        path: `${__dirname}/src/content/jidlo/`
       },
     },
     {
     resolve: 'gatsby-source-filesystem',
     options: {
         name: 'napoje',
-        path: `${__dirname}/content/napoje/`
+        path: `${__dirname}/src/content/napoje/`
     },
     }
   ],
