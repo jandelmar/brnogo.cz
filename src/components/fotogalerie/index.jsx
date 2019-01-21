7c4d59d719e5d5fda4f9d02ec7fe7b69ff88a518
Jan Delmar <jandelmardesign@gmail.com>
2019-01-21 20:34:49 +0100

    Add photo

--- src/components/fotogalerie/index.jsx
+++ src/components/fotogalerie/index.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
-export default ({ children }) => (
+import Photo from './photo'
+import photo from './photo';
+
+const photos = [
+    {
+        url: "https://unsplash.com/photos/Sy9-mUeW4jk",
+        alt: "nejaky alt"
+    },
+    {
+        url: "https://unsplash.com/photos/0TPmrjTXjSs",
+        alt: "nejaky alt"
+    },
+    {
+        url: "https://unsplash.com/photos/VSYjkfzj3nw",
+        alt: "nejaky alt"
+    },
+    {
+        url: "https://unsplash.com/photos/TOWN5qqOquw",
+        alt: "nejaky alt"
+    },
+    {
+        url: "https://unsplash.com/photos/LpYtdy59oxM",
+        alt: "nejaky alt"
+    },
+]
+
+export default ({ }) => (
     <React.Fragment>
-        <ul>
-            <li>fotka</li>
-            <li>fotka</li>
-            <li>fotka</li>
-        </ul>
+        for (photo of photo) {
+            <Photo src={photo.url} alt={photo.alt} />
+        }
     </React.Fragment>
 )
---
+++ src/components/fotogalerie/photo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default ({src, alt}) => (
+    <React.Fragment>
+        <image src="{src}" alt="{alt}" />
+    </React.Fragment>
+)
