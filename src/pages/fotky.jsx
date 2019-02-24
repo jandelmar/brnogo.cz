7ea2965b6c00fbca9197a8334379a6c07cb96ad2
Jan Delmar <jandelmardesign@gmail.com>
2019-02-24 21:42:27 +0100

    Fix urls

--- src/pages/fotky.jsx
+++ src/pages/fotky.jsx
@@ -12,62 +12,62 @@ export default () => (
 
 const photos = [
     {
-      src: "photos/Go-01_78932.jpg",
+      src: `${__dirname}photos/Go-01_78932.jpg`,
       width: 310,
       height: 414,
     },
     {
-      src: "photos/Go-02_78954.jpg",
+      src: `${__dirname}photos/Go-02_78954.jpg`,
       width: 310,
       height: 267,
     },
     {
-      src: "photos/Go-03_78975.jpg",
+      src: `${__dirname}photos/Go-03_78975.jpg`,
       width: 310,
       height: 405,
     },
     {
-      src: "photos/Go-04_78996.jpg",
+      src: `${__dirname}photos/Go-04_78996.jpg`,
       width: 310,
       height: 211,
     },
     {
-      src: "photos/Go-05_79024.jpg",
+      src: `${__dirname}photos/Go-05_79024.jpg`,
       width: 310,
       height: 414,
     },
     {
-      src: "photos/Go-06_79074.jpg",
+      src: `${__dirname}photos/Go-06_79074.jpg`,
       width: 310,
       height: 247,
     },
     {
-      src: "photos/Go-09_79137.jpg",
+      src: `${__dirname}photos/Go-09_79137.jpg`,
       width: 310,
       height: 240,
     },
     {
-      src: "photos/Go-11_79165.jpg",
+      src: `${__dirname}photos/Go-11_79165.jpg`,
       width: 310,
       height: 207,
     },
     {
-      src: "photos/Go-12_79187.jpg",
+      src: `${__dirname}photos/Go-12_79187.jpg`,
       width: 310,
       height: 232,
     },
     {
-      src: "photos/Go-13_79196.jpg",
+      src: `${__dirname}photos/Go-13_79196.jpg`,
       width: 310,
       height: 232,
     },
     {
-      src: "photos/Go-14_79209.jpg",
+      src: `${__dirname}photos/Go-14_79209.jpg`,
       width: 310,
       height: 232,
     },
     {
-      src: "photos/Go-15_79220.jpg",
+      src: `${__dirname}photos/Go-15_79220.jpg`,
       width: 310,
       height: 170,
     },
