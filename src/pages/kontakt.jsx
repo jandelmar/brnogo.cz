169fb0134a5b7176f5d8b6ddce8b9d6e0253f48b
Jan Delmar <jandelmardesign@gmail.com>
2019-05-16 19:11:19 +0200

    Update kontakt

--- src/pages/kontakt.jsx
+++ src/pages/kontakt.jsx
@@ -20,10 +20,7 @@ export default ({data}) => {
             <h1>Kontakt</h1>
             <Container fluid={true}>
                 <Row>
-                    <Col className="text-center mb-4">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.2473409205254!2d16.606829715686363!3d49.195867279322044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47129450a91a663d%3A0x29f036adfa900b22!2zR-G7lyBCcm5v!5e0!3m2!1scs!2scz!4v1558017004386!5m2!1scs!2scz" width="600" height="560" frameborder="0" allowfullscreen></iframe>
-                    </Col>
-                    <Col className="text-center text-md-left">
+                    <Col className="col-12 col-md-6 order-md-2 text-center text-md-left">
                         <p>{rezervace}</p>
                         <h2>otevírací doba</h2>
                         <p>
@@ -43,6 +40,9 @@ export default ({data}) => {
                         <p>
                             {provozovatel.map((line, i) => provozovatel.length === i+1 ? <span>{line}</span> : <span>{line}<br /></span>)}
                         </p>
+                    </Col>
+                    <Col className="col-12 col-md-6 order-md-1 text-center mb-4">
+                        <iframe className="embed-responsive" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2607.2473409205254!2d16.606829715686363!3d49.195867279322044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47129450a91a663d%3A0x29f036adfa900b22!2zR-G7lyBCcm5v!5e0!3m2!1scs!2scz!4v1558017004386!5m2!1scs!2scz" width="600" height="560" frameborder="0" allowfullscreen></iframe>
                     </Col>
                 </Row>
             </Container>
